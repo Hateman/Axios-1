d3b05b94ebd16d02a83671f58976f354d179202b
Alex <37542088+Hateman@users.noreply.github.com>
2019-03-18 22:06:48 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<!-- <div id="app">{{ info }}</div> -->
+<div id="app">
+  <h1>Bitcoin Price Index</h1>
+  <div
+    v-for="currency in info"
+    class="currency"
+  >
+    {{ currency.description }}:
+    <span class="lighten">
+      <span v-html="currency.symbol"></span>{{ currency.rate_float | currencydecimal }}
+    </span>
+  </div>
+</div>
+</template>
+
+<script>
+/* import axios from 'axios'
+export default {  
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => (this.info = response));
+  }
+  } */
+
+import axios from 'axios'
+export default {  
+  data() {
+    return {
+      info: null
+    };
+  },
+  mounted() {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response.data.bpi));
+  }
+  }
+
+</script>
